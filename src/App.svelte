738c3b71185967e61e7c2d951dd6793d04d241ce
u_taku0115 <ut1995j15@gmail.com>
2023-02-18 19:50:15 +0900

    ページ移植

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,92 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Translator from "./lib/translator";
+  const translator = new Translator();
+  let inputText: string = "";
+  let outputText: string = "";
+
+  function encodeText() {
+    const encoded = translator.encode(inputText);
+    outputText = encoded;
+    // console.log(encoded);
+  }
+
+  function decodeText() {
+    const decoded = translator.decode(inputText);
+    outputText = decoded;
+    // console.log(decoded);
+  }
 </script>
 
+
+<header style="text-align:center;">
+  <h1>ちんちんもーるす</h1>
+</header>
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+    <h4>ひらがなとカタカナをモールス信号に変えるよ！</h4>
+    <p>えんこーど: ぶんしょう → ちんちん...</p>
+    <p>でこーど: ちんちん... → ぶんしょう</p>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div>
+    <h3>にゅうりょく</h3>
+    <div>文字数: {inputText.length}</div>
+    <textarea
+      name="input-kana"
+      id="input-kana"
+      cols="30"
+      rows="10"
+      bind:value={inputText}
+    />
+  </div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div style="display:flex; justify-content:center;">
+    <button on:click={() => encodeText()}>エンコード</button>
+    <button on:click={() => decodeText()}>デコード</button>
+  </div>
+
+  <div>
+    <h3>しゅつりょく</h3>
+    <div>文字数: {outputText.length}</div>
+    <textarea
+      name="output-chin"
+      id="output-chin"
+      cols="30"
+      rows="10"
+      bind:value={outputText}
+      readonly
+    />
+  </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 500px;
+    margin: 0 auto;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  textarea {
+    resize: none;
+    width: 500px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  button {
+    width: 100px;
+    margin: 1rem;
   }
-  .read-the-docs {
-    color: #888;
+
+  @media (min-width: 640px) {
+    main,
+    header {
+      max-width: none;
+    }
+  }
+
+  @media (max-width: 500px) {
+    textarea {
+      width: 100%;
+    }
   }
 </style>
