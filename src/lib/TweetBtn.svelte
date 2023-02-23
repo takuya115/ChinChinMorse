908e2dcb79d89bfae6fb18293185951b1224d4af
u_taku0115 <ut1995j15@gmail.com>
2023-02-23 17:53:43 +0900

    aタグクリック時の挙動をスマホとPCで分ける

--- src/lib/TweetBtn.svelte
+++ src/lib/TweetBtn.svelte
@@ -5,15 +5,20 @@
 
     function getTweetLink(tweetText: string, shareLink: string): string {
         // モールスが120を超えるときは詰める
-        const tweetTextShorted = tweetText.length > 120 ? tweetText.slice(0, 120) + "..." : tweetText;
+        const tweetTextShorted =
+            tweetText.length > 120
+                ? tweetText.slice(0, 120) + "..."
+                : tweetText;
         const currentURL = location.href.split("?")[0];
-        let url = "https://twitter.com/share"
+        let url = "https://twitter.com/share";
         if (tweetText && shareLink) {
             url += `?text=${tweetTextShorted}${encodeURI("\n\n")}${shareLink}`;
         } else {
-            url += `?text=${encodeURI("ちんちん")}${encodeURI("\n\n")}${currentURL}`;
+            url += `?text=${encodeURI("ちんちん")}${encodeURI(
+                "\n\n"
+            )}${currentURL}`;
         }
-        return url
+        return url;
     }
 </script>
 
@@ -26,9 +31,29 @@
         nomodule
         src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
     ></script>
-    <a href={href} target="_blank" rel="noreferrer" class="tweet" title="ちんちんをツイート"
-        ><ion-icon name="logo-twitter" size="large" /></a
-    >
+    {#if navigator.userAgent.match(/iPhone|Android.+Mobile/)}
+    <!-- スマホの場合は別タブで開く -->
+        <a
+            {href}
+            target="_blank"
+            rel="noreferrer"
+            class="tweet"
+            title="ちんちんをツイート"
+            ><ion-icon name="logo-twitter" size="large" /></a
+        >
+    {:else}
+    <!-- PCの場合は別ウィンドウで開く -->
+        <a
+            href={null}
+            target="_blank"
+            rel="noreferrer"
+            class="tweet"
+            title="ちんちんをツイート"
+            on:click|preventDefault = {() => window.open(href, "ツイートする", "left=300,top=100,height=430,width=550")}
+            ><ion-icon name="logo-twitter" size="large" /></a
+        >
+    {/if}
+    
 </div>
 
 <style>
@@ -42,6 +67,6 @@
     .tweet {
         background: none;
         border: none;
-        cursor: pointer
+        cursor: pointer;
     }
 </style>
