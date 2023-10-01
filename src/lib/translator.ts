de4d702a8720adb8e0da583dd17cbde7b7937b3a
u_taku0115 <ut1995j15@gmail.com>
2023-10-01 12:14:18 +0900

    フォーマット

--- src/lib/translator.ts
+++ src/lib/translator.ts
@@ -7,7 +7,7 @@ export default class Translator {
     private separator: string;
     private setting: Object;
     private split: string;
-    
+
     constructor() {
         this.priority = 2
         this.dot = "ちんちん";
@@ -21,11 +21,11 @@ export default class Translator {
         }
         // 区切り文字は空白ではなくA(モールス信号で短いから)
         this.split = "A"
-        
+
     }
 
     // エンコード
-    public encode(plainText: string):string {
+    public encode(plainText: string): string {
         // 平文 -> uint8array -> モールス
         const array: Uint8Array = new TextEncoder().encode(plainText);
         return morse.encode(array.join(this.split), this.setting);
